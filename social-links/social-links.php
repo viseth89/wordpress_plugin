94794f532f7a9bb0edc2453c339e213d6c6b34bb
viseth89 <viseth89@gmail.com>
2019-03-21 18:44:16 -0500

    finished social links

--- social-links/social-links.php
+++ social-links/social-links.php
@@ -16,3 +16,11 @@ if (!defined('ABSPATH')) {
 require_once(plugin_dir_path(__FILE__) . '/includes/social-links-scripts.php');
 // Load Class
 require_once(plugin_dir_path(__FILE__) . '/includes/social-links-class.php');
+
+// Register Widget
+
+function register_social_links(){
+  register_widget('Social_Links_Widget');
+}
+
+add_action('widgets_init', 'register_social_links');
